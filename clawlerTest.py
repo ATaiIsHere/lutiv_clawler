14315a4ca855bf48bcc1072d56cae6935c5fac6a
sean.tai <sean.tai@bionime.com>
2019-08-07 22:36:27 +0800

    調整 parse 成 json 的 code 和畫資料分佈圖

--- clawlerTest.py
+++ clawlerTest.py
@@ -8,7 +8,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.use('TkAgg')
 
-
+# get url content
 def get_content(url):
     try:
         headers = {
@@ -25,8 +25,8 @@ def get_content(url):
         print("Error '%s' happened on line %d" % (s[1], s[2].tb_lineno))
         return " ERROR "
 
-
-def parse_try_report(content):
+# parse try report to json
+def parse_try_report_json(content, gender):
     try:
         soup = BeautifulSoup(content,'html.parser')
         row_datas = soup.find(name='tbody').find_all('tr')
@@ -37,15 +37,30 @@ def parse_try_report(content):
             rows.append(vals)
 
         heads = rows[0]
+        for i in range(len(heads)):
+            if "身高" in heads[i]:
+                heads[i] = "身高"
+            elif "體重" in heads[i]:
+                heads[i] = "體重"
+            elif "體重" in heads[i]:
+                heads[i] = "體重"
+            elif "胸圍" in heads[i]:
+                heads[i] = "胸圍"
+            elif "肩寬" in heads[i]:
+                heads[i] = "肩寬"
+            elif "腰圍" in heads[i]:
+                heads[i] = "腰圍"
+            elif "臀圍" in heads[i]:
+                heads[i] = "臀圍"
+            elif "身高" in heads[i]:
+                heads[i] = "身高"
+
         vals_list = rows[1:]
         result = []
 
-        # 過濾資料類型
-        if not (any("肩寬" in head for head in heads) and any("胸圍" in head for head in heads)):
-            return []
-
         for vals in vals_list:
             obj = {}
+            obj["gender"] = gender
             for i in range(len(heads)):
                 obj[heads[i]] = vals[i]
             result.append(obj)
@@ -57,37 +72,23 @@ def parse_try_report(content):
         return []
 
 
-def parse_try_report_v2(content):
+def parse_data_for_plot(data):
     try:
-        soup = BeautifulSoup(content,'html.parser')
-        row_datas = soup.find(name='tbody').find_all('tr')
-        rows = []
+        prop_a = "身高"
+        prop_b = "肩寬"
+        result = []
+        for category_data_list in data:
+            category_x = []
+            category_y = []
+            for dic in category_data_list:
+                if not(prop_a in dic and prop_b in dic and bool(dic[prop_a].strip()) and bool(dic[prop_b].strip())):
+                    continue
+                category_x.append(float(dic[prop_a]))
+                category_y.append(float(dic[prop_b]))
+            result.append([category_x, category_y])
 
-        for r in row_datas:
-            vals = list(map(lambda h: h.text, r.find_all('td')))
-            rows.append(vals)
-
-        heads = rows[0]
-        vals_list = rows[1:]
-
-        head_idx = [-1, -1]
-        # 過濾資料類型
-        for i in range(len(heads)):
-            if "肩寬" in heads[i]:
-                head_idx[0] = i
-            elif "胸圍" in heads[i]:
-                head_idx[1] = i
-
-        if -1 in head_idx:
-            return [], []
-
-        x = []
-        y = []
-        for vals in vals_list:
-            x.append(int(vals[head_idx[0]]))
-            y.append(int(vals[head_idx[1]]))
-
-        return x, y
+        print(result)
+        return result[0], result[1]
     except:
         s = sys.exc_info()
         print("Error '%s' happened on line %d" % (s[1], s[2].tb_lineno))
@@ -97,50 +98,52 @@ def parse_try_report_v2(content):
 def show_scatter_plot(class_a, class_b):
     plt.scatter(class_a[0], class_a[1], color='blue')
     plt.scatter(class_b[0], class_b[1], color='red')
+    plt.ylabel('Shoulder width')
+    plt.xlabel('bust')
 
     plt.show()
 
 
 def main():
-    # content = get_content(try_report_url + '43333')
-    # print(content)
-    # parseTryReport(content)
-    # return
     data = []
+    if not os.path.isfile('lutiv.json'):
+        for category in categorys:
+            url_without_page_index = product_list_url + category
+            content = ''
+            product_list = []
+            category_data = []
 
-    for category in categorys:
-        url_without_page_index = product_list_url + category
-        content = ''
-        product_list = []
-        category_data = [[], []]
+            page_index = 0
+            while content != '[]':
+                url = url_without_page_index + '&pageIndex=' + str(page_index)
+                content = get_content(url)
+                products = json.loads(content)
+                product_list = product_list + products
+                print('Page Index' + str(page_index) + ' - 資料筆數：' + str(len(products)))
+                page_index = page_index + 1
 
-        page_index = 0
-        while content != '[]':
-            url = url_without_page_index + '&pageIndex=' + str(page_index)
-            content = get_content(url)
-            products = json.loads(content)
-            product_list = product_list + products
-            print('Page Index' + str(page_index) + ' - 資料筆數：' + str(len(products)))
-            page_index = page_index + 1
+            print('總筆數：' + str(len(product_list)))
 
-        print('總筆數：' + str(len(product_list)))
+            for p in product_list:
+                style_no = p["image_140"].split("/")[2]
+                print('Get Data, Style No: ' + style_no + '...')
+                category_data = category_data + parse_try_report_json(get_content(try_report_url + style_no), category)
 
-        for p in product_list:
-            style_no = p["image_140"].split("/")[2]
-            print('Get Data, Style No: ' + style_no + '...')
-            x, y = parse_try_report_v2(get_content(try_report_url + style_no))
-            category_data[0] = category_data[0] + x
-            category_data[1] = category_data[1] + y
-        data.append(category_data)
+            data.append(category_data)
 
-    with open('data.json', 'w') as f:
-        json.dump(data, f)
-    show_scatter_plot(data[0], data[1])
+        with open('lutiv.json', 'w', encoding='utf8') as f:
+            json.dump(data, f, ensure_ascii=False)
+    else:
+        with open('lutiv.json', 'r', encoding='utf8') as f:
+            data = json.load(f)
+
+    men, women = parse_data_for_plot(data)
+    show_scatter_plot(men, women)
     # po = pandas.read_json(json.dumps(data))
-    # po.to_csv('/Users/Sean.Tai/PycharmProjects/lutiv_clawler/women.csv', encoding='utf_8_sig')
+    # po.to_csv('/Users/Sean.Tai/PycharmProjects/lutiv_clawler/lutiv.csv', encoding='utf_8_sig')
 
 
-# 接口地址
+# 資料 url
 product_list_url = 'https://www.lativ.com.tw/Product/GetNewProductCategoryList?MainCategory='
 try_report_url = 'https://www.lativ.com.tw/Product/TryReport?styleNo='
 categorys = ['MEN', 'WOMEN']
